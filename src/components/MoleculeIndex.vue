fea65831e6b731da98768c84fd8e5c5fd0c322b0
jumei <1191068887@qq.com>
2023-02-22 17:16:42 +0800

    search，smiles apply

--- src/components/MoleculeIndex.vue
+++ src/components/MoleculeIndex.vue
@@ -1,26 +1,57 @@
 <template>
   <div class="molecule">
-    <div class="left_content">
-      <a-tabs :type="'card'" v-model="activeKey" @change="changeTab">
-        <a-tab-pane key="ketcher" tab="Ketcher"/>
-        <a-tab-pane key="jsme" tab="JSME"/>
-      </a-tabs>
-      <iframe class="frame" id="idKetcher" src="./standalone/index.html" width="800" height="600" v-show="activeKey==='ketcher'"/>
-      <JSME height="600px" width="800px" options="oldlook,star,atommovebutton,hydrogens" :model-value="jsmeSmiles" :onChange="changeSmiles" v-show="activeKey==='jsme'"/>
-    </div>
-    <div class="right_content">
-      <p>
-        <a-button @click="getSmiles" type="primary" icon="search">
-          Get SMILES
-        </a-button>
-      </p>
-      <p style="font-size: 14px">{{ activeKey==='ketcher'?ketcherSmiles:jsmeSmiles }}</p>
+    <p>
+      <a-input-search class="input-search" v-model="smiles"
+                      placeholder="input smiles text"
+                      size="large"
+                      @search="onSearch"
+      >
+        <template #enterButton>
+          <a-button>Apply</a-button>
+        </template>
+      </a-input-search>
+    </p>
+    <div class="flex">
+      <div class="left_content">
+        <a-tabs :type="'card'" v-model="activeKey" @change="changeTab">
+          <a-tab-pane key="ketcher" tab="Ketcher"/>
+          <a-tab-pane key="jsme" tab="JSME"/>
+        </a-tabs>
+        <iframe class="frame" id="idKetcher" src="./standalone/index.html" width="800" height="600"
+                v-show="activeKey==='ketcher'"/>
+        <JSME height="600px" width="800px" options="oldlook,star,atommovebutton,hydrogens" :model-value="jsmeSmiles"
+              :onChange="changeSmiles" v-show="activeKey==='jsme'"/>
+      </div>
+      <div class="right_content">
+        <h2>搜索条件</h2>
+        <a-radio-group class="radio-group" v-model="searchKey">
+          <a-radio :value="1">子结构搜索</a-radio>
+          <a-radio :value="2">精确搜索</a-radio>
+          <a-radio :value="3">90%相似搜索</a-radio>
+          <a-radio :value="4">60%相似搜索</a-radio>
+          <a-radio :value="5">30%相似搜索</a-radio>
+        </a-radio-group>
+        <p>
+          <a-button @click="queryRecords" type="primary" icon="search">
+            Search Results
+          </a-button>
+        </p>
+        <a-divider />
+        <p>
+          <a-button @click="getSmiles" type="primary" icon="search">
+            Get SMILES
+          </a-button>
+        </p>
+        <p style="font-size: 14px">{{ activeKey === 'ketcher' ? ketcherSmiles : jsmeSmiles }}</p>
+      </div>
     </div>
   </div>
+
 </template>
 
 <script>
 import JSME from "./JSME";
+import { Modal } from 'ant-design-vue';
 
 export default {
   name: 'MoleculeIndex',
@@ -29,14 +60,23 @@ export default {
   },
   data() {
     return {
+      searchKey: 1,
       activeKey: 'ketcher',
       ketcher: null,
       ketcherSmiles: null,
       jsmeSmiles: null,
       currentSmiles: null,
+      smiles: null,
     }
   },
   methods: {
+    onSearch() {
+      this.$nextTick(() => {
+        this.initKetcher();
+        this.ketcher.setMolecule(this.smiles)
+        this.jsmeSmiles = this.smiles
+      })
+    },
     changeSmiles(val) {
       this.currentSmiles = val;
     },
@@ -59,7 +99,7 @@ export default {
       this.ketcherSmiles = ''
       this.jsmeSmiles = ''
     },
-    initKetcher(){
+    initKetcher() {
       let ketcherFrame = document.getElementById('idKetcher');
       let ketcher = null;
       if ('contentDocument' in ketcherFrame) {
@@ -68,12 +108,18 @@ export default {
         ketcher = document.frames['idKetcher'].window.ketcher;
       }
       this.ketcher = ketcher;
+    },
+    queryRecords() {
+      Modal.info({
+        title: () => '温馨提示',
+        content: () => '该功能为高级版功能，请联系微信： zccbbg',
+      });
     }
   },
   created() {
     this.$nextTick(() => {
       setTimeout(() => {
-       this.initKetcher();
+        this.initKetcher();
       }, 500)
     })
   },
@@ -83,8 +129,15 @@ export default {
 
 <style scoped>
 .molecule {
-  display: flex;
   padding: 30px;
+}
+
+.input-search {
+  width: 800px;
+}
+
+.flex {
+  display: flex;
 }
 
 .molecule .left_content {
@@ -92,7 +145,16 @@ export default {
 }
 
 .molecule .right_content {
+  width: calc(100% - 800px);
   margin-left: 50px;
   margin-top: 50px;
+}
+
+.radio-group {
+  display: grid;
+}
+
+.ant-radio-wrapper {
+  padding: 10px 0 !important;
 }
 </style>
